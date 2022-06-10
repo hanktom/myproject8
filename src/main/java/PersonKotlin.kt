d90808d226a8794a027935c5311dad4578107eb3
Hank Tom <hanktom@gmail.com>
2022-06-10 12:55:36 +0800

    Kotlin hello

---
+++ src/main/java/PersonKotlin.kt
@@ -0,0 +1,13 @@
+package com.tom
+
+class PersonKotlin {
+    fun greeting() {
+        println("Hello kotlin")
+    }
+}
+
+//1st level
+fun main() {
+    val p = PersonKotlin()
+    p.greeting()
+}
